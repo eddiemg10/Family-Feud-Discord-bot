62bb91154bdb64d3912dce4231d8412412f5f613
eddiemg10 <59659920+eddiemg10@users.noreply.github.com>
2022-02-04 18:28:12 +0300

    commands

--- index.js
+++ index.js
@@ -1,14 +1,55 @@
 // Require the necessary discord.js classes
-const { Client, Intents } = require('discord.js');
+const fs = require('fs');
+const { Client, Collection, Intents } = require('discord.js');
 const { token } = require('./config.json');
 
 // Create a new client instance
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
-// When the client is ready, run this code (only once)
-client.once('ready', () => {
-	console.log('Ready!');
-});
+client.commands = new Collection();
+
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	// Set a new item in the Collection
+	// With the key as the command name and the value as the exported module
+	client.commands.set(command.data.name, command);
+}
+
+for (const file of eventFiles) {
+	const event = require(`./events/${file}`);
+	if (event.once) {
+		client.once(event.name, (...args) => event.execute(...args));
+	} else {
+		client.on(event.name, (...args) => event.execute(...args));
+	}
+}
+
+// const command = require('./commands');
+
+// //permissions: 543522552896
+
+// // When the client is ready, run this code (only once)
+// client.on('ready', () => {
+// 	console.log('Ready!');
+// });
+
+// client.on('interactionCreate', async interaction => {
+// 	if (!interaction.isCommand()) return;
+
+// 	const { commandName } = interaction;
+
+// 	if (commandName === 'ping') {
+// 		await interaction.reply('Pong!');
+// 	} else if (commandName === 'server') {
+// 		await interaction.reply(`Server name: ${interaction.guild.name}\nTotal members: ${interaction.guild.memberCount}`);
+// 	} else if (commandName === 'user') {
+// 		await interaction.reply(`Your tag: ${interaction.user.tag}\nYour id: ${interaction.user.id}`);
+// 	}
+// });
+
 
 // Login to Discord with your client's token
 client.login(token);
